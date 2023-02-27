a71baf263073e56ae64b8a1c10c54c9565df3cd0
naseehan <106654509+naseehan@users.noreply.github.com>
2023-02-27 11:41:32 +0530

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -17,7 +17,7 @@ function Navbar() {
       </li>
       
       <li className="nav-item active">
-        <a className="nav-link " href="/reviews">Reviews</a>
+        <a className="nav-link " href="./reviews">Reviews</a>
       </li>
     </ul>
     <form className="form-inline my-2 my-lg-0">
